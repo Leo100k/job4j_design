4cdbc49f8060f69412b016ffe3ed0a2579734af3
Leo100k <lemail2004@mail.ru>
2024-01-15 21:12:08 +0300

    0.1. Statement [#379306]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,133 @@
+package ru.job4j.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws IOException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws IOException {
+        Properties config = new Properties();
+        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("app.properties")) {
+            config.load(in);
+            String url = config.getProperty("url");  /*jdbc:postgresql://localhost:5432/some_db*/
+            String username = config.getProperty("username"); /*postgres*/
+            String password = config.getProperty("password"); /*root*/
+
+            connection = DriverManager
+                    .getConnection(url, username, password);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void createTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "CREATE TABLE IF NOT EXISTS %s (%s, %s);", tableName,
+                    "id SERIAL PRIMARY KEY",
+                    "name TEXT"
+            );
+            statement.execute(sql);
+            System.out.println(getTableScheme(tableName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void dropTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "DROP TABLE IF EXISTS %s", tableName);
+            statement.execute(sql);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void addColumn(String tableName, String columnName, String typeColumn) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE " + tableName + " ADD " + columnName + " " + typeColumn);
+            statement.execute(sql);
+            System.out.println(getTableScheme(tableName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE " + tableName + " DROP COLUMN " + columnName);
+            statement.execute(sql);
+            System.out.println(getTableScheme(tableName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE " + tableName + " RENAME COLUMN " + columnName + " TO " + newColumnName);
+            statement.execute(sql);
+            System.out.println(getTableScheme(tableName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public String getTableScheme(String tableName) throws Exception {
+        System.out.println("Из getTableScheme " + tableName);
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "SELECT * FROM %s LIMIT 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties prop = new Properties();
+        TableEditor tableEditor = new TableEditor(prop);
+        tableEditor.createTable("test_demo");
+        tableEditor.addColumn("test_demo", "address", "varchar(100)");
+        tableEditor.renameColumn("test_demo", "address", "location");
+        tableEditor.dropColumn("test_demo", "location");
+        tableEditor.dropTable("test_demo");
+
+    }
+
+}
+
